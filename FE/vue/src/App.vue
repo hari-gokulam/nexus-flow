8c180d67caf2caec3cb3bb87fcff3d4b7558c5b2
hari-gokulam <hari.gokulam@gmail.com>
2025-08-23 23:34:18 +0530

    vue initial skeleton version

---
+++ FE/vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <main class="section" >
+      <div class="container">
+        <TopBar/>
+       </div>
+    </main>
+</template>
+
+<script>
+import TopBar from './components/TopBar.vue';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    TopBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ FE/vue/src/components/TopBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="top-bar">
+        <div class="top-bar-left">
+            Logo
+        </div>
+        <div class="top-bar-right">
+            <div class="top-bar-right-item">
+                
+            </div>
+            <div class="top-bar-right-item">
+                
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TopBar"
+    }
+</script>
